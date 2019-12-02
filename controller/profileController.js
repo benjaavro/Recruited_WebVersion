2cf8041ab9b6b93eb59519db12def1a86ec0dc4c
Luz <luz.catzoli@gmail.com>
2019-12-02 13:56:27 -0600

    Código Benja

--- controller/profileController.js
+++ controller/profileController.js
@@ -34,9 +34,20 @@ class ProfileController {
         });
     }
 
-    updateDataCoach(data) {
+    uupdateDataCoach(data) {
         console.log("updating to backend");
         return new Promise((resolve, reject) => { axios.post('http://10.25.241.57:3000/profile/CoachEdit',data).then(function(success) {
+            console.log(success);
+            resolve(success.data);
+        }).catch(function(err){
+            console.log(err);
+            reject(err);
+        })
+        });
+    }
+
+    getAthleteStats(data) {
+        return new Promise((resolve, reject) => { axios.post('http://10.25.241.57:3000/stats/Get',data).then(function(success) {
             console.log(success);
             resolve(success.data);
         }).catch(function(err){
