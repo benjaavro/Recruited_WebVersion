c9ff5ad952aff110eb1197f1b16c9f1cb451c5f5
Benjamin Avila Rosas <benjaavro@gmail.com>
2019-12-02 11:21:51 -0600

    Edit profile ready

--- controller/profileController.js
+++ controller/profileController.js
@@ -1,6 +1,18 @@
 class ProfileController {
     getDataAthlete(data) {
-        return new Promise((resolve, reject) => { axios.post('http://192.168.1.76:3000/profile/Athlete',data).then(function(success) {
+        return new Promise((resolve, reject) => { axios.post('http://10.25.241.57:3000/profile/Athlete',data).then(function(success) {
+            console.log(success);
+            resolve(success.data);
+        }).catch(function(err){
+            console.log(err);
+            reject(err);
+        })
+        });
+    }
+
+    updateDataAthlete(data) {
+        console.log("updating to backend");
+        return new Promise((resolve, reject) => { axios.post('http://10.25.241.57:3000/profile/AthleteEdit',data).then(function(success) {
             console.log(success);
             resolve(success.data);
         }).catch(function(err){
