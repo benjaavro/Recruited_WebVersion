52d61a0b8b0a2159e83af042903a1ada5529105a
Alex <alvaso97@hotmail.com>
2019-12-01 23:14:10 -0600

    v4

--- Backend/node/model/profileModel.js
+++ Backend/node/model/profileModel.js
@@ -20,6 +20,23 @@ class profileModel {
         })
     }
 
+    getCoachProfile(usr) {
+        console.log(usr);
+        const sql = `SELECT * FROM coach WHERE idCoach = ?`
+        const params = [usr.Id];
+        return new Promise((resolve, reject) => {
+            this.db.query(sql, params, function (err, res) {
+                if (err) {
+                    reject(err)
+                } else {
+                    console.log("Respuesta");
+                    console.log(res);
+                    resolve(res)
+                }
+            })
+        })
+    }
+
     editAthleteProfile(usr) {
         console.log(usr);
         const sql = `UPDATE athlete SET name = ? , age = ?, phoneNumber = ?, address = ? , institution = ?, description = ?, sport = ? ,password = ?, Institution_idInstitution = (SELECT idInstitution FROM institution WHERE name = ? ) WHERE idAthlete = ?`
@@ -37,7 +54,7 @@ class profileModel {
         })
     }
 
-    getCoachProfile(usr) {
+    editCoachProfile(usr) {
         console.log(usr);
         const sql = `UPDATE coach SET name = ? , phoneNumber = ?, address = ? , institution = ?, description = ?, password = ?, Institution_idInstitution = (SELECT idInstitution FROM institution WHERE name = ? ) WHERE idCoach = ?`
         const params = [usr.Name,usr.Phone,usr.Location,usr.Institution,usr.Institution, usr.Description,usr.Password,usr.Institution,usr.Id];
